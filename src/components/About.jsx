7a50da5559700d030a7d31b69507c7dff6b8ba31
Jimeno0 <carlospj@icloud.com>
2017-01-10 14:14:31 +0100

    Set brand color, about section

---
+++ src/components/About.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const About = () =>
+  <section className="about">
+    <h2>WHO?</h2>
+    <p>Hi Im Carlos P. Jimeno AKA <a href="https://twitter.com/jimeno0">jimeno0</a></p>
+    <p>Im a full stack developer and new techs lover. Self-taught addicted.</p>
+    <p>
+      Currently working at <a href="https://influencity.com/es/"> Influencity </a>
+      and a <a href="https://facebook.github.io/react/">React</a> fanboy.
+    </p>
+    <h2>WHY?</h2>
+    <p>I first studied Suervey engineer cause I love maps.</p>
+    <p>then a GIS master cause I really really love maps.</p>
+    <p>then I discover code and I didnt care about maps anymore.</p>
+    <p>
+      I course the <a href="https://www.ironhack.com/es/curso-programacion-web">Ironhack webdev bootcamp</a> and now I dont do anything but code JS.</p>
+  </section>;
+export default About;
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Intro from './intro';
 import NavButton from './nav_button';
+import About from './About';
 
 const App = () =>
   <div>
     <Intro />
     <NavButton />
+    <About />
   </div>;
 export default App;
 
