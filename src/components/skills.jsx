9d9cf8e266e9324c70ab5e888566676ac662d5e7
Jimeno0 <carlospj@icloud.com>
2017-01-11 17:29:35 +0100

    Style skills section & footer

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -2,14 +2,24 @@ import React from 'react';
 
 const Skills = () =>
   <section className="skills">
-    <h3>FRONT</h3>
-    <p>Javascript · HTML · CSS</p>
-    <h3>BACK</h3>
-    <p>Rails · Node</p>
-    <h3>LOVE</h3>
-    <p>React · Redux · Webpack · Sass ·   Git · Postman</p>
-    <h3>DESIGN</h3>
-    <p>Sketch · Illustrator</p>
+    <h2>SKILLS</h2>
+    <div>
+      <h3>FRONT</h3>
+      <p>Javascript · HTML · CSS</p>
+    </div>
+    <div>
+      <h3>BACK</h3>
+      <p>Rails · Node</p>
+    </div>
+    <div>
+      <h3>LOVE</h3>
+      <p>React · Redux · Webpack · Sass ·   Git · Postman</p>
+    </div>
+    <div>
+      <h3>DESIGN</h3>
+      <p>Sketch · Illustrator</p>
+    </div>
+    <hr />
   </section>;
 
 export default Skills;
