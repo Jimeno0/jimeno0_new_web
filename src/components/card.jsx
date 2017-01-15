c4d4bcfb29c6df7ef89f259a56ef376f387af299
Jimeno0 <carlospj@icloud.com>
2017-01-15 11:52:53 +0100

    Set · separator at skills and projects sections

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -2,12 +2,18 @@ import React from 'react';
 
 const Card = (props) => {
   const { name, image, description, technologies, github, web } = props.project;
+  const technologiesStyled = technologies.map(tech =>
+    <span>
+      {tech}
+    </span>,
+  );
   return (
     <article>
       <h3>{name}</h3>
       <a href={web}><img src={image} alt={name} /></a>
       <p>{description}</p>
-      <p className="technologies">{technologies}</p>
+      {/* <p className="technologies">{technologies}</p> */}
+      <p className="technologies">{technologiesStyled}</p>
       <a href={github}>
         <img className="icon" src="src/images/github-logo.svg" alt="github link" />
       </a>
--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -5,19 +5,19 @@ const Skills = () =>
     <h2>SKILLS</h2>
     <div>
       <h3>FRONT</h3>
-      <p>Javascript · HTML · CSS</p>
+      <p>Javascript<span> · </span>HTML<span> · </span>CSS</p>
     </div>
     <div>
       <h3>BACK</h3>
-      <p>Rails · Node</p>
+      <p>Rails<span> · </span>Node</p>
     </div>
     <div>
       <h3>LOVE</h3>
-      <p>React · Redux · Webpack · Sass ·   Git · Postman</p>
+      <p>React<span> · </span>Redux<span> · </span>Webpack<span> · </span>Sass<span> · </span>Git<span> · </span>Postman</p>
     </div>
     <div>
       <h3>DESIGN</h3>
-      <p>Sketch · Illustrator</p>
+      <p>Sketch<span> · </span>Illustrator</p>
     </div>
     <hr />
   </section>;
